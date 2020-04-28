2a1f7e40e0999ee99a05961df2bd081fcda0222d
Oleksandr Sapalsky <fav755@gmail.com>
2020-04-28 17:17:20 +0300

    Fixed images pathes

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -4,6 +4,7 @@
 class ArticlePreview extends HTMLElement {
   connectedCallback() {
     this.className = 'content_article';
+    
     fetch(`resources/articles/${this.dataset.articleName}.html`)
       .then((response) => response.text())
       .then((text) => this.innerHTML = text);
