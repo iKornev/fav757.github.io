c09c670fc734cb47c8b7d03cff2c3d3cc61cafdd
Oleksandr Sapalsky <fav755@gmail.com>
2020-04-28 21:18:06 +0300

    Added top menu change color event

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,4 +1,6 @@
 'use strict';
+// Page elements variables
+const topMenu = document.body.querySelector('.top-menu');
 
 // Discribes how article could be shown
 class ArticlePreview extends HTMLElement {
@@ -12,3 +14,16 @@ class ArticlePreview extends HTMLElement {
 }
 
 customElements.define('app-article-preview', ArticlePreview);
+
+// Change top menu color on scroll
+document.addEventListener('scroll', function () {
+  if (window.pageYOffset) {
+    topMenu.style.background = 'white';
+    topMenu.style.color = 'black';
+    topMenu.style.boxShadow = '0 2px 2px gray';
+  } else {
+    topMenu.style.background = '';
+    topMenu.style.color = '';
+    topMenu.style.boxShadow = '';
+  }
+});
