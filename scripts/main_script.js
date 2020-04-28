96166219ae9b316adfa41881f52062e14dc1aa96
Oleksandr Sapalsky <fav755@gmail.com>
2020-04-28 17:14:27 +0300

    Changed file structure and article class

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -4,17 +4,9 @@
 class ArticlePreview extends HTMLElement {
   connectedCallback() {
     this.className = 'content_article';
-    this.innerHTML = `<div class="content_article_preview-img">
-    <img src="resources/images/preview_for_artcicle1.jpg" alt="Превью новости"></img>
-  </div>
-
-  <div class="content_article_preview-text">
-    <h3>Кулинары предупреждают: борщ не прощает ошибок</h3>
-    <p>Борщ считается одним из главных блюд у разных народов, и имеет множество рецептов приготовления. Но
-      кулинары
-      уверены, что в любом случае при его приготовлении непростительны ошибки, передает «МИР 24»</p>
-    <p>This article is about ${this.dataset.articleName}</p>
-  </div>`;
+    fetch(`resources/articles/${this.dataset.articleName}.html`)
+      .then((response) => response.text())
+      .then((text) => this.innerHTML = text);
   }
 }
 
