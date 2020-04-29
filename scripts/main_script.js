2ab729b59764291cc16df31464fb8f4ec8cc937a
Oleksandr Sapalsky <fav755@gmail.com>
2020-04-29 13:22:03 +0300

    Update to article loader class

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -19,23 +19,43 @@ document.addEventListener('scroll', function () {
 class ArticlesPreview extends HTMLElement {
   constructor() {
     super();
+    ArticlesPreview.articlesDB.sort((a, b) => a.date - b.date);
+    this.loadArticles();
+  }
 
+  // Load +3 not used articles
+  async loadArticles() {
     for (let i = 0; i < 2; i++) {
-      fetch('resources/articles/borsh.html')
+      let notUserArticle;
+
+      for (let article of ArticlesPreview.articlesDB) {
+        if (!this.querySelector(`[name="${article.name}"]`)) {
+          notUserArticle = article;
+          break;
+        }
+      }
+
+      let htmlText;
+
+      await fetch(`resources/articles/${notUserArticle.name}.html`)
         .then((response) => response.text())
-        .then((html) => this.insertAdjacentHTML('beforeEnd', html))
+        .then((html) => htmlText = html)
+
+      this.insertAdjacentHTML('beforeEnd', htmlText);
+      this.lastElementChild.setAttribute('name', notUserArticle.name)
     }
   }
 
+  // Database with articles info
   static articlesDB = [
     {
       name: 'borsh',
-      date: new Date('05/28/2020'),
+      date: new Date('05/11/2020')
     },
 
     {
       name: 'vino',
-      date: new Date('05/27/2020')
+      date: new Date('05/13/2020')
     }
   ];
 }
